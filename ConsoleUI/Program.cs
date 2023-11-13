165dfaf0628b896ac85c94d8fac53c7e8603b109
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-11-13 09:25:00 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using Business.Concretes;
+using Entities.Concretes;
+
+Category category1= new Category();
+category1.Id = 1;
+category1.Name = "Programlama";
+
+CategoryManager categoryManager = new CategoryManager();
+categoryManager.Add(category1);
+
+
+//Sıfırdan proje oluşturunuz --> TobetoETrade
+//Product sınıfını katmanlarında işletiniz.
+//Product --> Id,Name,Unitprice
